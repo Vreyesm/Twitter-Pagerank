5783fab666c3f413ea95dd7abe21623de18667e7
Victor Reyes Medina <victor.reyes.medina@gmail.com>
2020-02-05 02:29:37 -0300

    pagerank test

--- Pagerank.py
+++ Pagerank.py
@@ -1,29 +1,44 @@
 from io_lib import *
 from User import *
+import pickle
 
+users = {}
 
-if __name__ == '__main__':
-    files_path = './data/'
-    labels_file = 'labels.csv'
-    edges_file = 'edges.csv'
+#
+# @return True if has loaded data from previous file, otherwise, False
+#
+def load_users():
+    global users
+    try:
+        with open (files_path + 'users.pkl', 'rb') as users_input:
+            print('Loading previous users data')
+            users = pickle.load(users_input)
+            print('Loaded previous users data')
+        return True
+    except FileNotFoundError:
+        print('Generating dictionary of [key: user_tag]')
+        with open (files_path + labels_file, 'r', encoding='utf8') as f:
+            for line in f:
+                line = line.rstrip()
+                values = line.split(';')
+                key = values[0]
+                tag = values[1]
+                user = User(key, tag, [])
+                
+                users[key] = user            
+            print (f'Number of users: {len(users)}')
+        for key in users:
+            user = users[key]
+            if (key != user.key):
+                prin('error')
+        return False
 
-    users = {}
+def load_edges():
+    global users
 
-    ensure_dir(files_path)
+    users_output_file = 'users.pkl'
 
-    with open (files_path + labels_file, 'r', encoding='utf8') as f:
-        for line in f:
-            line = line.rstrip()
-            values = line.split(';')
-            key = values[0]
-            tag = values[1]
-            user = User(key, tag)
-            #print(f'KEY: {key}\nTag:{tag}')
-            
-            users[key] = user
-        
-        print (f'Number of users: {len(users)}')
-    
+    print('Reading nodes from file')
     with open (files_path + edges_file, 'r', encoding='utf8') as f:
         
         edges_counter = 0
@@ -33,22 +48,80 @@ if __name__ == '__main__':
             line = line.rstrip()
             values = line.split(';')
 
-            user_follower_key = values[1]
-            user_followed_key = values[0]
+            user_follower_key = values[0]
+            user_followed_key = values[1]
 
             if (user_followed_key not in users):
-                new_user = User(user_followed_key, user_followed_key)
-                users[user_followed_key] = new_user
+                #new_user = User(user_followed_key, user_followed_key)
+                #users[user_followed_key] = new_user
+                continue
             if (user_follower_key not in users):
-                new_user = User(user_follower_key, user_follower_key)
-                users[user_follower_key] = new_user
+                #new_user = User(user_follower_key, user_follower_key)
+                #users[user_follower_key] = new_user
+                continue
 
             followed = users[user_followed_key]
             follower = users[user_follower_key]
             followed.followers.append(follower)
-
-    print (f'Number of edges: {edges_counter}')
-    print (f'Final number of users: {len(users)}')
+        print (f'Number of edges: {edges_counter}')
+        print (f'Final number of users: {len(users)}')
     
+    with open (files_path + users_output_file, 'wb') as users_output:
+        pickle.dump(users, users_output, pickle.HIGHEST_PROTOCOL)
+
+
+def PR(user, N, d = 0.85):
+    base = (1 - d) / N
+
+    sum = 0
+    #print(f'User {user.tag} has {len(user.followers)} followers')
+    for in_user in user.followers:
+        sum += in_user.rank
+    return base + sum
+
+
+if __name__ == '__main__':
+    files_path = './data/'
+    labels_file = 'labels.csv'
+    edges_file = 'edges.csv'
+
+    ensure_dir(files_path)
+
+
+    print('Number of iterations', end=': ')
+    n = int(input())
+
+    if ( not load_users() ):
+        load_edges()
+
+
+    n_users = len(users)
+    
+    
+
+    initial_rank = 1.0 / len(users)
+    print(f'Initial rank for all users {initial_rank}')
+
+    print('Initializing users rank')
+    for key in users:
+        user = users[key]
+        user.rank = initial_rank
+        
+        #print(f'User {user.tag} has {len(user.followers)} followers')
+
+    exit
+
+
+    iteration = 0
+    while iteration < n:
+        for key in users:
+            user = users[key]
+            user.rank = PR(user, n_users)
+        i += 1
+
+    users_list = [ u for u in users.values ]
+    users_list.sort(key=lambda u: u.rank, reverse=True)
+
+
     input("Press enter to finish")
     
--- User.py
+++ User.py
@@ -1,6 +1,7 @@
 class User:
 
-    def __init__(self, key, tag, followers = []):
+    def __init__(self, key, tag, followers = [], rank = 0):
         self.key = key
         self.tag = tag
         self.followers = followers
+        self.rank = rank
